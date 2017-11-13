8b07903f922c20b1bfcdfae277ffa83edfec1fa7
Ron Barrantes <ronbarrantes@gmail.com>
2017-11-12 18:14:14 -0800

    working on the oauth router

--- lab-ron/backend/route/auth-router.js
+++ lab-ron/backend/route/auth-router.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const {Router} = require('express');
-const Account = require('../model/account.js');
+const { Router } = require('express');
+const superagent = require('superagent');
 const httpErrors = require('http-errors');
+const Account = require('../model/account.js');
 const basicAuth = require('../lib/basic-auth-middleware.js');
 
 const authRouter = module.exports = new Router();
@@ -11,17 +12,56 @@ authRouter.post('/auth', (req, res, next) => {
   Account.create(req.body)
     .then(account => account.tokenCreate())
     .then(token => {
-      res.cookie('X-CharityChoice-Token', token, {maxAge: 604800000});
-      res.json({token});
+      res.cookie('X-CharityChoice-Token', token, { maxAge: 604800000 });
+      res.json({ token });
     })
     .catch(next);
+});
+
+authRouter.get('/oauth/google', (req, res, next) => {
+  console.log(req.query);
+  if (!req.query.code) {
+    res.redirect(process.env.CLIENT_URL);
+  } else {
+    superagent.post('https://www.googleapis.com/oauth2/v4/token')
+      .type('form')
+      .send({
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri: `${process.env.API_URL}/oauth/google`,
+      })
+      .then((res) => {
+        console.log(res.body);
+        if (!res.body.accessToken)
+          throw new Error('no access token');
+        return res.body.access_token;
+      })
+      .then(accessToken => {
+        return superagent.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect')
+          .set('Authorization', `Bearer ${accessToken}`);
+      })
+      .then(res => {
+        console.log('--> openID profile', res.body);
+      })
+      .then(() => {
+        res.redirect(process.env.CLIENT_URL);
+      })
+      .catch(err => {
+        console.error(err);
+        res.redirect(process.env.CLIENT_URL);
+      });
+  }
+
+
 });
 
 authRouter.get('/auth', basicAuth, (req, res, next) => {
   req.account.tokenCreate()
     .then(token => {
-      res.cookie('X-CharityChoice-Token', token, {maxAge: 604800000});
-      res.json({token});
+      res.cookie('X-CharityChoice-Token', token, { maxAge: 604800000 });
+      res.json({ token });
     })
     .catch(next);
 });
